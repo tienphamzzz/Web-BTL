7a7e341fad9df81c781418ed8923259fdd09cfaf
Doanh <viaicamon28@gmail.com>
2024-09-29 22:58:03 +0700

    Change Views

---
+++ Web-BTL/Models/LogInModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Web_BTL.Models
+{
+    public class LogInModel // dùng để khi đăng nhập và đăng kí dễ dùng ko bị gò bó bởi cái trường dữ liệu khác của User
+    {
+        public string? Email;
+        public string? Password;
+    }
+}
