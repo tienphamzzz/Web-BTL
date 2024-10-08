1f7c4bbe7c5a0260fc3f4eb66aaf290f9f28bca1
Tiền Phạm <tienpham2kk4@gmail.com>
2024-10-08 16:18:59 +0700

    make index for genre

--- Web-BTL/Controllers/GenreController.cs
+++ Web-BTL/Controllers/GenreController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_BTL.Repository;
 
 namespace Web_BTL.Controllers
 {
     public class GenreController : Controller
     {
+        private DataContext db;
+        public GenreController(DataContext _db)
+        {
+            db = _db;
+        }
         public IActionResult Index()
         {
-            return View();
+            var media = db.Medias.Include(m => m.Genres).ToList();
+            return View(media);
         }
     }
 }
---
+++ Web-BTL/Controllers/MediaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Web_BTL.Models.Medias;
+using Web_BTL.Repository;
+
+namespace Web_BTL.Controllers
+{
+    public class MediaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
