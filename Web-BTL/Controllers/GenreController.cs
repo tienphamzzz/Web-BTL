c61b5e717b612720c95bcce550aaa542db1fe8f2
Tiền Phạm <tienpham2kk4@gmail.com>
2024-10-12 21:57:13 +0700

    commit

--- Web-BTL/Controllers/GenreController.cs
+++ Web-BTL/Controllers/GenreController.cs
@@ -10,7 +10,6 @@ namespace Web_BTL.Controllers
     public class GenreController : Controller
     {
         private DataContext db;
-        private int PageSize = 16;
         public GenreController(DataContext _db)
         {
             db = _db;
@@ -18,6 +17,11 @@ namespace Web_BTL.Controllers
         public IActionResult Index()
         {
             var media = db.Medias.Include(m => m.Genres).ToList();
+            // Lấy danh sách thể loại từ cơ sở dữ liệu
+            var genres = db.Genres.ToList();
+
+            // Gán danh sách thể loại vào ViewBag
+            ViewBag.Genre = genres;
             return View(media);
         }
 
@@ -48,6 +52,24 @@ namespace Web_BTL.Controllers
 
             // Trả về View cùng với danh sách các MediaModel của trang hiện tại
             return View(result);
+        }
+
+        public IActionResult MoviesFilter(int? mid)
+        {
+            int PageSize = 8;
+            var medias = (IQueryable<MediaModel>)db.Medias;
+
+            if (mid != null)
+            {
+                medias = (IQueryable<MediaModel>)db.Medias
+                    .Where(m => m.Genres.Any(g => g.GenreId == mid));
+
+                ViewBag.mid = mid;
+            }
+
+            var result = medias.Take(PageSize).ToList();
+
+            return PartialView("_MediaPartial", result);
         }
 
     }
