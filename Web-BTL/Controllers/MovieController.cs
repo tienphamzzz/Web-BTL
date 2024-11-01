656d8e9ad00cb2e85d09907e43ea598de10dc8a6
viaicamon <152273744+viaicamon@users.noreply.github.com>
2024-11-01 22:54:53 +0700

    Update MovieController.cs

--- Web-BTL/Controllers/MovieController.cs
+++ Web-BTL/Controllers/MovieController.cs
@@ -15,7 +15,27 @@ namespace Web_BTL.Controllers
         public IActionResult Index(int movieId)
         {
             var movie = db.Medias.FirstOrDefault(m => m.MediaId == movieId);
+            var customer = db.Customers.FirstOrDefault(c => c.UserEmail == HttpContext.Session.GetString("LogIn Session"));
+            if (customer == null) return RedirectToAction("SignIn", "Account");
+                var watchListId = customer.WatchListId;
 
+                bool isAlreadyInList = db.ListMedia
+                    .Any(lm => lm.WatchListId == customer.WatchListId && lm.MediaId == movieId);
+            if (!isAlreadyInList)
+            {
+
+                var newListMedia = new ListMediaModel
+                {
+                    WatchListId = customer.WatchListId,
+                    MediaId = movieId,
+                    IsWatched = false,
+                    Favorite = false,
+                    AddDate = DateTime.Now
+                };
+
+                db.ListMedia.Add(newListMedia);
+                db.SaveChanges();
+            }
             if (movie == null)
             {
                 return NotFound(); // If the movie does not exist, return a 404 error
@@ -25,17 +45,32 @@ namespace Web_BTL.Controllers
             return View(movie);
         }
         public IActionResult watching(int movieId)
+        {
+            var movie = db.Medias.FirstOrDefault(m => m.MediaId == movieId);
+            var customer = db.Customers.FirstOrDefault(c => c.UserEmail == HttpContext.Session.GetString("LogIn Session"));
+            var listMedia = db.ListMedia
+                .FirstOrDefault(lm => lm.WatchListId == customer.WatchListId && lm.MediaId == movieId);
+                listMedia.IsWatched = true;
+            db.SaveChanges();
+            if (movie == null)
             {
-                var movie = db.Medias.FirstOrDefault(m => m.MediaId == movieId);
-
-                if (movie == null)
-                {
-                    return NotFound(); // If the movie does not exist, return a 404 error
-                }
+                return NotFound(); // If the movie does not exist, return a 404 error
+            }
 
                 // Pass the movie object to the view
-                return View(movie);
-            }
+            return View(movie);
         }
+        [HttpPost]
+        public IActionResult AddFavourite(int mid)
+        {
+            var customer = db.Customers.FirstOrDefault(c => c.UserEmail == HttpContext.Session.GetString("LogIn Session"));
     
+            var listMedia = db.ListMedia
+                .FirstOrDefault(lm => lm.WatchListId == customer.WatchListId && lm.MediaId == mid);
+            if (listMedia == null) return NotFound();
+            listMedia.Favorite = true;
+            db.SaveChanges();
+            return PartialView("AddFavourite");
+        }
+    }
 }
