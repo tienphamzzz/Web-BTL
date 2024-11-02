eb86fe9519fcf67da021ddb76682588b04eb4a49
viaicamon <152273744+viaicamon@users.noreply.github.com>
2024-11-02 20:56:42 +0700

    Add files via upload

---
+++ Web-BTL/ViewComponents/PackageViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web_BTL.Repository;
+
+namespace Web_BTL.ViewComponents
+{
+    public class PackageViewComponent : ViewComponent
+    {
+        private readonly DataContext _dataContext;
+        public PackageViewComponent(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<string> package = new List<string> { "Basic", "Premium", "Vip" };
+            return View("Default", package);
+        }
+    }
+}
